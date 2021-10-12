9fbd4c53e5b732d6f431ab34075582b46190afd1
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-10-12 09:28:26 -0400

    adds and styles footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,12 +7,20 @@ import github from "./images/github.png";
 import "./Footer.css";
 
 function Footer() {
+   const twitterURL = "https://twitter.com/";
+   const linkedinURL = "https://www.linkedin.com/in/jamesvazquez01/";
+   const githubURL = "https://github.com/Jvaz0715";
    return (
       <div className="footer">
-         <img src={twitter} className="contact-logos" />
-         <img src={linkedin} className="contact-logos"/>
-         <img src={github} className="contact-logos" />
-
+         <div className="logos-container">
+            <a href={twitterURL} target="_blank"><img src={twitter} className="contact-logos" /></a>
+            <a href={linkedinURL} target="_blank"><img src={linkedin} className="contact-logos"/></a>
+            <a href={githubURL} target="_blank"><img src={github} className="contact-logos" /></a>
+         </div>
+         <div className="copyright">
+            <p>Copyright 2021</p>
+         </div>
+         
       </div>
    )
 };
